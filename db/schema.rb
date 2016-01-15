2b86e6f263f6a94437eee5743b9d15d139b16bf9
cjavier086 <carlos.jimenez@pucp.pe>
2016-01-14 19:00:48 -0500

    Implement Home View

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,14 @@
 ActiveRecord::Schema.define(version: 20150113013628) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
-    t.string   "remember_digest"
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
+    t.string   "remember_digest", limit: 255
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
