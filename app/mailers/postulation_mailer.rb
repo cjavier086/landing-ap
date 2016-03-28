a041620e3a586b970142aaaf3260210fad25189a
DeluxeAlonso <alonso.alvarez@pucp.pe>
2016-03-28 05:42:48 -0500

    design

--- app/mailers/postulation_mailer.rb
+++ app/mailers/postulation_mailer.rb
@@ -3,6 +3,8 @@ class PostulationMailer < ApplicationMailer
 
 	def get_postulation(postulation)
 		@postulation = postulation
+		p "HHHHHHHHH"
+		p @postulation.owner
 		mail(to: "digitecnia.peru@gmail.com",
 	 	      :from => "support@mydomain.com", subject: "Solicitud de Postulación Número #{@postulation.id.to_s}")
 	end
