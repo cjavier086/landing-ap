32761dc51f74765718a4c1c7b07172c9d59f3b48
DeluxeAlonso <alonso.alvarez@pucp.pe>
2016-04-29 17:49:38 -0500

    Reclamation book mailer

--- app/mailers/book_mailer.rb
+++ app/mailers/book_mailer.rb
@@ -1,11 +1,10 @@
 class BookMailer < ApplicationMailer
 	default from: "digitecnia@gmail.com"
 
-	def new_message(book){
-		@book = book
-		p "HHHHHHHHH"
-		mail(to: "carlos.jimenez@pucp.pe",
-	 	      :from => "support@mydomain.com", subject: "Solicitud de Postulación Número #{@reclamo.id.to_s}")
-		
-	}
+	def get_book(book)
+	  @book = book
+	  p "HHHHHHHHH"
+	  mail(to: "digitecnia.peru@gmail.com",
+	 	   :from => "support@mydomain.com", subject: "Solicitud de Reclamación Número #{@book.id.to_s}")	
+	end
 end
